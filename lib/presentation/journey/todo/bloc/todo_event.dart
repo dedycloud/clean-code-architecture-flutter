1fe000122d775fda21974e316911675f8f3f544e
dedy <dedyindra351@gmail.com>
2021-03-31 12:51:58 +0700

    todo-app

--- lib/presentation/journey/todo/bloc/todo_event.dart
+++ lib/presentation/journey/todo/bloc/todo_event.dart
@@ -1,1 +1,29 @@
-abstract class TodoEvent {}
+part of 'todo_bloc.dart';
+
+abstract class TodoEvent  {}
+class GetTodo extends TodoEvent {
+  GetTodo();
+
+  List<Object> get props => [];
+}
+
+class CreateTodo extends TodoEvent {
+  final String description;
+  CreateTodo({this.description});
+
+  List<Object> get props => [description];
+}
+
+class DeleteTodo extends TodoEvent {
+  final String id;
+  DeleteTodo({this.id});
+
+  List<Object> get props => [id];
+}
+
+class UpdateTodo extends TodoEvent {
+  final String id;
+  UpdateTodo({this.id,});
+
+  List<Object> get props => [id];
+}
--- lib/presentation/journey/todo/bloc/todo_state.dart
+++ lib/presentation/journey/todo/bloc/todo_state.dart
@@ -1,3 +1,27 @@
+part of 'todo_bloc.dart';
+
 abstract class TodoState {}
 
+
 class TodoInitial extends TodoState {}
+class TodoLoading extends TodoState {}
+class TodoError extends TodoState {}
+
+class TodoLoaded extends TodoState {
+  final List<TodoEntity> todoList;
+
+
+  TodoLoaded({
+    this.todoList
+  });
+
+  @override
+  List<Object> get props => [ todoList
+
+  ];
+}
+
+class CreateTodoSuccess extends TodoState {}
+class DeleteTodoSuccess extends TodoState {}
+class UpdateTodoSuccess extends TodoState {}
+
